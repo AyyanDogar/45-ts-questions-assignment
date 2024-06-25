c00504c17451ba3ae4cbd5f2d49220320e3e4bfb
AYYAN 40 <163725087+AyyanDogar@users.noreply.github.com>
2024-06-25 03:02:59 +0000

    testing

---
+++ index.ts
@@ -0,0 +1,62 @@
+//Array list Dynamic typed language 
+// let array = ['ayyan','hamza',50,true]
+// console.log(array[1]);
+// let num = [1,2,3,4,5,6,7,8,9]
+
+//to add an element in array at the end
+// num.push(10)
+// console.log(num);
+//to remove an element from array
+// num.pop()
+// console.log(num);
+//slice an array
+// let num3 = num.slice(-1)
+// console.log(num3);
+
+//splicing an array
+// num.splice(2,3,40,41,39)
+// console.log(num);
+
+
+//practice
+// let lira=["hamza", "rafi","ayyan","pheta"]
+// lira.push("ali zain")
+// console.log(lira);
+// let gonePerson=lira.pop()
+// console.log(gonePerson);
+// lira.splice(2,1,'abdullah')
+// console.log(lira);
+// lira.unshift("penny" )
+// lira.shift();
+// console.log(lira)
+
+// function isPass(x:number){   
+                                       
+//     if(x>=500)
+//         return x
+// }
+
+
+// let marks : number[]=[]
+// marks.unshift(881)
+// marks.push(54)
+// marks.unshift(40)
+// marks.push(57847487)
+// console.log(marks);
+// let marks1 = marks.filter((e)=>{return e>=500})
+
+// console.log(marks1);
+
+//Arrow function
+// setTimeout(()=>{console.log("hello ");
+// },10000);
+
+// setInterval(()=>{console.log("hello ");
+// },20)
+
+// const func = (x)=>{
+
+// }
+
+// Write a loop that logs numbers from 1 to 50.
+//  For multiples of 3, log "Fizz" instead of the number, and for multiples of 5, log "Buzz".
